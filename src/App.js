80d6f576bede2c60ed4ba84a52fec53b7b0d3974
Brett Seifried <seifriedbrett@gmail.com>
2022-02-22 09:57:35 -0800

    set up auth for Login.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { ProvideAuth } from './context/AuthContext';
 import PrivateRoute from './context/PrivateRoute';
 import { UserProvider } from './context/UserContext';
 import Home from './services/views/Home';
@@ -8,16 +9,18 @@ import Login from './services/views/Login';
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <PrivateRoute path="/">
-            <Home />
-          </PrivateRoute>
-        </Switch>
-      </BrowserRouter>
+      <ProvideAuth>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <PrivateRoute path="/">
+              <Home />
+            </PrivateRoute>
+          </Switch>
+        </BrowserRouter>
+      </ProvideAuth>
     </UserProvider>
   );
 }
--- src/services/views/Login.jsx
+++ src/services/views/Login.jsx
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
+import { useAuth } from '../../hooks/useAuth';
+import { useForm } from '../../hooks/useForm';
 
 export default function Login() {
-  const [name, setName] = useState();
-  const [password, setPassword] = useState();
+  const { formState, handleFormChange } = useForm({ email: '', password: '' });
+  const auth = useAuth();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const loginSucessful = auth.login(formState.email, formState.password);
+  };
 
   return (
     <>
       <div>
         <label>github.com/</label>
       </div>
-      <div>
-        <input
-          type="text"
-          value={name}
-          placeholder="github user name"
-          onChange={(e) => setName(e.target.value)}
-        ></input>
+      <form onSubmit={handleLogin}>
         <div>
-          <label>Password</label>
+          <input type="text" id="name" name="name" placeholder="github user name"></input>
+          <div>
+            <label>Password</label>
+          </div>
+          <input id="password" type="password" name="password" placeholder="Not Github Password" />
         </div>
-        <input type="password" value={password} placeholder="Not Github Password" />
-      </div>
+        <button type="submit">Log in</button>
+      </form>
     </>
   );
 }
