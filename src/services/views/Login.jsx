4a7bacd57a79412ddd63808d93d46b16c831cf78
Brett Seifried <seifriedbrett@gmail.com>
2022-02-22 10:05:40 -0800

    Can type into input, button isn't working

--- src/services/views/Login.jsx
+++ src/services/views/Login.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import { useForm } from '../../hooks/useForm';
 
 export default function Login() {
   const { formState, handleFormChange } = useForm({ email: '', password: '' });
   const auth = useAuth();
+  const [error, setError] = useState(null);
+  const history = useHistory();
 
   const handleLogin = (e) => {
     e.preventDefault();
     const loginSucessful = auth.login(formState.email, formState.password);
+    !loginSucessful ? setError('Incorrect Login') : history.push('/');
   };
 
   return (
@@ -18,14 +22,29 @@ export default function Login() {
       </div>
       <form onSubmit={handleLogin}>
         <div>
-          <input type="text" id="name" name="name" placeholder="github user name"></input>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={formState.email}
+            placeholder="github user name"
+            onChange={handleFormChange}
+          ></input>
           <div>
             <label>Password</label>
           </div>
-          <input id="password" type="password" name="password" placeholder="Not Github Password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={formState.password}
+            placeholder="Not Github Password"
+            onChange={handleFormChange}
+          />
         </div>
         <button type="submit">Log in</button>
       </form>
+      {/* {error} */}
     </>
   );
 }
