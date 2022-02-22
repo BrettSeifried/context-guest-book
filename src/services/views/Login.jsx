562026ca7eb3a0261465aa33403706cbee70c95f
Brett Seifried <seifriedbrett@gmail.com>
2022-02-21 17:00:28 -0800

    login page rendering inputs

--- src/services/views/Login.jsx
+++ src/services/views/Login.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
-  return <div>Login page</div>;
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
+
+  return (
+    <>
+      <div>
+        <label>github.com/</label>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={name}
+          placeholder="github user name"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <div>
+          <label>Password</label>
+        </div>
+        <input type="password" value={password} placeholder="Not Github Password" />
+      </div>
+    </>
+  );
 }
