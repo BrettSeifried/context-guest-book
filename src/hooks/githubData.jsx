303853d92c65fb6b852c47d1f7f6593bddeb2e5b
Brett Seifried <seifriedbrett@gmail.com>
2022-02-22 12:33:42 -0800

    mesasge updates with gitHub info

--- src/hooks/githubData.jsx
+++ src/hooks/githubData.jsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from 'react';
+import { useUser } from '../context/UserContext';
 import { fetchGithub } from '../services/apiData/route';
 
-export default function useGithubData(name) {
+export default function useGithubData() {
   const [userName, setUserName] = useState('');
   const [image, setImage] = useState('');
   const [link, setLink] = useState('');
   const [repo, setRepo] = useState('');
+  const { user } = useUser();
 
   useEffect(() => {
     async function getProfile() {
-      const profile = await fetchGithub(name);
-      setUserName(profile.name);
+      const profile = await fetchGithub(user);
+      setUserName(user);
       setImage(profile.avatar_url);
       setLink(profile.html_url);
       setRepo(profile.public_repos);
     }
     getProfile();
-  }, [name]);
+  }, [user]);
 
   return { userName, image, link, repo };
 }
